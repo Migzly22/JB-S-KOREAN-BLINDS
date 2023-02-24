ce2f16c494b887243cbabc87e541cb34faded62b
Migzly22 <rolly.migrino02@gmail.com>
2023-02-24 13:15:24 +0800

    fix

--- JS/main.js
+++ JS/main.js
@@ -1,24 +1,7 @@
 import imgdata from './data.json' assert {type:'json'} 
 
 
-const showsmallnav = document.getElementById('showsmallnav')
-showsmallnav.addEventListener('click',(e)=>{
-    e.preventDefault()
-    const smallnav = document.getElementsByClassName('smallnav')[0]
-    const mainbodycover = document.getElementsByClassName("mainbodycover")[0]
-    const mainnav = document.getElementsByClassName("mainnav")[0]
 
-    if (!smallnav.classList.contains("smallnavactive")){
-        smallnav.classList.add("smallnavactive")
-        mainnav.classList.add("smallmove")
-        mainbodycover.style.display = "block"
-    }else{
-        smallnav.classList.remove("smallnavactive")
-        mainnav.classList.remove("smallmove")
-        mainbodycover.style.display = "none"
-    }
-
-})
 
 /**Swiper */
 const swiper = new Swiper('.swiper', {
